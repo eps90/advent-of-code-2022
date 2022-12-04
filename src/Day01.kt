0f3b6dca4f2666b3a7cea39478b87567d96fe5dd
Kuba Turek <ja@kubaturek.pl>
2022-12-04 18:12:17 +0100

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,30 @@
 fun main() {
+    fun caloriesPerElf(input: List<String>): List<Int> {
+        return input
+            .fold(mutableListOf()) { acc, s ->
+                if (s.isEmpty()) {
+                    acc.add(0)
+                    acc
+                } else {
+                    val lastElem = acc.lastOrNull() ?: 0
+                    val newAcc = acc.dropLast(1).toMutableList()
+                    newAcc.add(lastElem + s.toInt())
+                    newAcc
+                }
+            }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return caloriesPerElf(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return caloriesPerElf(input).sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
